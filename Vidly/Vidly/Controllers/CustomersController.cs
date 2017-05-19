0d71721cc1d2e88096497755608c9f0ce828cc17
Leander Clappaert <leander.clappaert@student.odisee.be>
2017-05-20 01:24:34 +0300

    Vidly Section 2 exercise finished

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> customerlist = new List<Customer>();
+
+        public CustomersController()
+        {
+            customerlist.Add(new Customer { Name = "John Smith", Id = 1 });
+            customerlist.Add(new Customer { Name = "Mary Williams", Id = 2 });
+        }
+
+        // GET: Customers
+        [Route("Customers")]
+        public ActionResult Customers()
+        {
+            var viewModel = new CustomerViewModel
+            {
+                CustomerList = customerlist
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            string customerName = "";
+
+            foreach (var customer in customerlist)
+            {
+                if (customer.Id == id)
+                {
+                    customerName = customer.Name;
+                    break;
+                }
+            }
+
+            if (customerName == "")
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var viewModel = new CustomerDetailViewModel
+                {
+                    Name = customerName,
+                    Id = id
+                };
+
+                return View(viewModel);
+            }
+        }
+    }
+}
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        [Route("Movies")]
+        public ActionResult Movies()
+        {
+            List<Movie> movielist = new List<Movie>();
+            movielist.Add(new Movie{ Name = "Shrek", Id = 1});
+            movielist.Add(new Movie { Name = "Wall-e", Id = 2 });
+
+            var viewModel = new MovieViewModel
+            {
+                MovieList = movielist
+            };
+
+            return View(viewModel);
+        }
+
+        /*[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }*/
+    }
+}
