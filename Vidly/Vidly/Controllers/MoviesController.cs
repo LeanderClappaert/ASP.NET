5efa49e743b2d0d218897abe48f8a3de4bd101c7
Leander Clappaert <leander.clappaert@student.odisee.be>
2017-05-23 23:25:44 +0300

    Section 3, exercise 3 finished

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         [Route("Movies")]
         public ActionResult Movies()
         {
-            List<Movie> movielist = new List<Movie>();
-            movielist.Add(new Movie{ Name = "Shrek", Id = 1});
-            movielist.Add(new Movie { Name = "Wall-e", Id = 2 });
+            var movielist = _context.Movies.Include(m => m.Genre).ToList();
 
             var viewModel = new MovieViewModel
             {
@@ -23,10 +34,30 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-        /*[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        [Route("Movies/Details/{id}")]
+        public ActionResult Details(int id)
         {
-            return Content(year + "/" + month);
-        }*/
+            var movielist = _context.Movies.Include(m => m.Genre).ToList();
+
+            if (movielist == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var movie = movielist.Find(c => c.Id == id);
+
+                var viewModel = new MovieDetailViewModel
+                {
+                    Name = movie.Name,
+                    Genre = movie.Genre,
+                    ReleaseDate = movie.ReleaseDate,
+                    DateAdded = movie.DateAdded,
+                    NumberInStock = movie.NumberInStock
+                };
+
+                return View(viewModel);
+            }
+        }
     }
 }
---
+++ Vidly/Vidly/ViewModels/MovieDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class MovieDetailViewModel
+    {
+        public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public byte NumberInStock { get; set; }
+    }
+}
